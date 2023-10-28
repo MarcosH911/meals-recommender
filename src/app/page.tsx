3bcdf640edd07b8d990b1b011d03e70922a4cc53
MarcosH911 <marcosh.craft@gmail.com>
2023-10-28 12:02:17 +0200

    fixed refactoring bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 // Spoontacular
 
-import WeightForm from "../components/custom/weight-form/WeightForm";
-
 function Page() {
-  return (
-    <div className="mx-auto max-w-md py-8">
-      <WeightForm />
-    </div>
-  );
+  return <div className="mx-auto max-w-md py-8">{/* <WeightForm /> */}</div>;
 }
 
 export default Page;
