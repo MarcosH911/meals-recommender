086f5eec5563c1384ab6e40f45fbf4fc99d26ced
unknown <marcosh.craft@gmail.com>
2023-11-06 08:03:17 +0100

    changed loading spinner

--- src/app/(auth)/components/AuthButtonSubmit.tsx
+++ src/app/(auth)/components/AuthButtonSubmit.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Loader2Icon } from "lucide-react";
+import { LoaderIcon } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 interface Props {
@@ -17,7 +17,7 @@ function AuthButtonSubmit({ children }: Props) {
         className="flex w-full items-center justify-center gap-2 bg-orange-500 text-orange-50 hover:bg-orange-600 dark:bg-orange-500 dark:text-orange-50 dark:hover:bg-orange-600"
         disabled={pending}
       >
-        {pending && <Loader2Icon className="h-4.5 w-4.5 animate-spin" />}
+        {pending && <LoaderIcon className="h-4.5 w-4.5 animate-spin" />}
         {children}
       </Button>
     </div>
